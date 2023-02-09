48733ca032fa53360526193a0f3cfdf2f07fffce
Tatiana Tkachenko <tkachenko.tatiana16@gmail.com>
2023-02-09 20:29:01 +0100

    chore: add husky

--- src/utils/onError.ts
+++ src/utils/onError.ts
@@ -1,6 +1,6 @@
-import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export const onError = (err: any, req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) => {
+export const onError = (err: any, req: NextApiRequest, res: NextApiResponse) => {
   console.error(err.stack);
   res.status(500).end('Something broke!');
 };

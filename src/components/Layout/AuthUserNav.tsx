ce031a79b0a3b80b1d18c5b520e0b60143da4b7d
Tatiana Tkachenko <tkachenko.tatiana16@gmail.com>
2023-05-14 11:19:31 +0200

    feat: create layout

---
+++ src/components/Layout/AuthUserNav.tsx
@@ -0,0 +1,16 @@
+import { Box, Button } from '@mui/material';
+import Link from 'next/link';
+
+const pages = [{ title: 'Orders', href: '/orders' }];
+
+export const AuthUserNav = () => {
+  return (
+    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, ml: 2 }}>
+      {pages.map((page) => (
+        <Button key={page.title} href={page.href} component={Link}>
+          {page.title}
+        </Button>
+      ))}
+    </Box>
+  );
+};
